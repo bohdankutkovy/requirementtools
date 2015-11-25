4c6341d7c0e87252b230d6b14bc1a3d8b308ab13
Bohdan Kutkovy <bohdan.kutkovy@sigma.software>
2015-11-25 20:09:09 +0200

    Requirement action show test covered

--- spec/contorllers/rquirements_controller_spec.rb
+++ spec/contorllers/rquirements_controller_spec.rb
@@ -27,7 +27,7 @@ describe RequirementsController, type: :controller do
 
     describe 'GET index'do
 
-      it 'redirects after index action' do
+      it 'get index page' do
         get :index
         expect(response.status).to eq(200)
       end
@@ -37,6 +37,22 @@ describe RequirementsController, type: :controller do
         r2 = FactoryGirl.create(:requirement, author_id: user.id, :project_id=> 1, parent_id: r1.id)
         get :index, :project_id => 1
         expect(assigns(:requirements)).to eq([r1])
+      end
+
+    end
+
+    describe 'GET show'do
+
+      it 'get show page' do
+        FactoryGirl.create(:requirement, author_id: user.id, :project_id=> 1)
+        get :show, :id => 1
+        expect(response.status).to eq(200)
+      end
+
+      it 'assigns @requirement with passed requirement id' do
+        r1 = FactoryGirl.create(:requirement, author_id: user.id, :project_id=> 1)
+        get :show, :id => r1.id
+        expect(assigns(:requirement)).to eq(r1)
       end
 
     end
