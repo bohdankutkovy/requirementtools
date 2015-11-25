22eb76583ad05067bb56ddfb65d142ff1b00e002
Bohdan Kutkovy <bohdan.kutkovy@sigma.software>
2015-11-25 13:44:59 +0200

    Requirement controller inclusion of filters tested

--- spec/contorllers/rquirements_controller_spec.rb
+++ spec/contorllers/rquirements_controller_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe RequirementsController, type: :controller do
     load File.join(Rails.root, 'app/controllers/requirements_controller.rb')
   end
 
-  it 'includes sign_in filter' do
-
+  it 'includes sign_in && entry_accessed filters' do
+    RequirementsController.should_receive(:before_filter).with(:ensure_user_is_signed_in?)
+    RequirementsController.should_receive(:before_filter).with(:entry_accessed?)
+    load File.join(Rails.root, 'app/controllers/requirements_controller.rb')
   end
 
 
