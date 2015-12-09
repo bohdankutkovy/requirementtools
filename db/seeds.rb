d445673a12bd89c8383a81ef2937677a9bcb2188
Bohdan Kutkovy <bohdan.kutkovy@sigma.software>
2015-12-09 15:02:10 +0200

    Admin user creation added to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+User.create(:email => "admin@example.com",
+             :name => "JulesAbrams",
+             :password => "adminpassword",
+             :confirmed_at => DateTime.now,
+             :is_super_admin => true,
+             :info_edited => false)
 #   Mayor.create(name: 'Emanuel', city: cities.first)
