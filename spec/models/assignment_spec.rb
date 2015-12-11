2274b39dd402fee8e36a09bddf04fd0b1d9ff783
Bohdan Kutkovy <bohdan.kutkovy@sigma.software>
2015-12-11 17:30:50 +0200

    Assignment model test covered

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -56,6 +56,36 @@ describe Assignment do
         user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 3)
         expect(user_assignment.available_for_read?(user)).to eq(true)
       end
+
+      it 'allows user to update assignment if current user acl > assignments acl' do
+        user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 0)
+        some_user = FactoryGirl.create(:user)
+        some_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: some_user.id, acl_level: 3)
+        expect(some_assignment.available_for_update?(user)).to eq(true)
+
+        #acl_level = 1 > assignment.acl = 3
+        user_assignment.update_attributes(acl_level: 1)
+        expect(some_assignment.available_for_update?(user)).to eq(true)
+
+        #acl_level = 2 > assignment.acl = 3
+        user_assignment.update_attributes(acl_level: 2)
+        expect(some_assignment.available_for_update?(user)).to eq(true)
+      end
+
+      it 'allows user to update assignment if current user acl > assignments acl' do
+        user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 0)
+        some_user = FactoryGirl.create(:user)
+        some_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: some_user.id, acl_level: 3)
+        expect(some_assignment.available_for_delete?(user)).to eq(true)
+
+        #acl_level = 1 > assignment.acl = 3
+        user_assignment.update_attributes(acl_level: 1)
+        expect(some_assignment.available_for_delete?(user)).to eq(true)
+
+        #acl_level = 2 > assignment.acl = 3
+        user_assignment.update_attributes(acl_level: 2)
+        expect(some_assignment.available_for_delete?(user)).to eq(true)
+      end
     end
 
     context 'assigned with poor rights' do
@@ -63,6 +93,41 @@ describe Assignment do
         user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 3)
         expect(user_assignment.available_for_create?(user)).to eq(false)
       end
+
+      it 'doesnt allow to update assignments if current_user.acl < assignments.acl' do
+        user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 3)
+        some_user = FactoryGirl.create(:user)
+        some_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: some_user.id, acl_level: 0)
+        expect(some_assignment.available_for_update?(user)).to eq(false)
+
+        #acl_level = 2 < assignment.acl = 1
+        user_assignment.update_attributes(acl_level: 2)
+        some_assignment.update_attributes(acl_level: 1)
+        expect(some_assignment.available_for_update?(user)).to be_nil
+
+        #acl_level = 1 < assignment.acl = 0
+        user_assignment.update_attributes(acl_level: 1)
+        some_assignment.update_attributes(acl_level: 0)
+        expect(some_assignment.available_for_update?(user)).to be_nil
+      end
+
+      it 'doesnt allow to delete assignments if current_user.acl < assignments.acl' do
+        user_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: user.id, acl_level: 3)
+        some_user = FactoryGirl.create(:user)
+        some_assignment = FactoryGirl.create(:assignment, project_id: project.id, user_id: some_user.id, acl_level: 0)
+        expect(some_assignment.available_for_delete?(user)).to eq(false)
+
+        #acl_level = 2 < assignment.acl = 1
+        user_assignment.update_attributes(acl_level: 2)
+        some_assignment.update_attributes(acl_level: 1)
+        expect(some_assignment.available_for_delete?(user)).to eq(false)
+
+        #acl_level = 1 < assignment.acl = 0
+        user_assignment.update_attributes(acl_level: 1)
+        some_assignment.update_attributes(acl_level: 0)
+        expect(some_assignment.available_for_delete?(user)).to eq(false)
+      end
+
     end
 
 
